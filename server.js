ed62a53a84a7870e382cd3f23565cf4dc95306e2
seen2 <tiwarietc@gmail.com>
2022-01-09 15:33:15 +0530

    Base Code

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ mongoose
   .catch((err) => console.log("error:---------->" + err.message));
 
 // sever start
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server started on: " + port));
